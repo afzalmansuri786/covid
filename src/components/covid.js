83831d38b73f81c230b642bc74d8b623f8e1636c
Uniking <mastark3000@gmail.com>
2021-08-18 20:55:21 +0530

    New Layout

--- src/components/covid.js
+++ src/components/covid.js
@@ -2,6 +2,9 @@
 import React,  { useEffect, useState } from "react";
 // import { Row } from "reactstrap";
 import "./covid.css";
+import $ from "jquery";
+
+
 
 const Covid = () => {
 
@@ -10,11 +13,11 @@ const Covid = () => {
 
     const getCovidData = async () => {
         try{
-            const res = await fetch('https://api.covid19india.org/data.json');
+            const res = await fetch("https://data.covid19india.org/data.json");
             const actualData = await res.json();
-            // console.log(actualData);
-            // console.log(actualData);
-            // console.log(actualData.statewise);
+            console.log(actualData);
+            console.log(actualData);
+            console.log(actualData.statewise);
             setData(actualData.statewise);
         }catch(err){
             console.log("error");
@@ -54,6 +57,97 @@ const Covid = () => {
         //     </tr>;
             
         // }
+
+
+
+        function sortTable(f,n){
+            var rows = $('#mytable tbody  tr').get();
+        
+            rows.sort(function(a, b) {
+        
+                var A = getVal(a);
+                var B = getVal(b);
+                
+                if(A < B) {
+                    return -1*f;
+                }
+                if(A > B) {
+                    return 1*f;
+                }
+                return 0;
+            });
+        
+            function getVal(elm){
+                var v = $(elm).children('td').eq(n).text().toUpperCase();
+                if($.isNumeric(v)){
+                    v = parseInt(v,10);
+                }
+                return v;
+            }
+        
+            $.each(rows, function(index, row) {
+                $('#mytable').children('tbody').append(row);
+            });
+        }
+        var f_sl = 1;
+        var f_nm = 1;
+        // $("#nm").click(function(){
+        //     f_sl *= -1;
+        //     var n = $(this).prevAll().length;
+        //     sortTable(f_sl,n);
+        // });
+        $("#cnf").click(function(){
+            f_sl *= -1;
+            var n = $(this).prevAll().length;
+            sortTable(f_sl,n);
+        });
+        $("#act").click(function(){
+            f_nm *= -1;
+            var n = $(this).prevAll().length;
+            sortTable(f_nm,n);
+        });
+        $("#rcv").click(function(){
+            f_nm *= -1;
+            var n = $(this).prevAll().length;
+            sortTable(f_nm,n);
+        });
+        $("#dth").click(function(){
+            f_nm *= -1;
+            var n = $(this).prevAll().length;
+            sortTable(f_nm,n);
+        });
+
+        $(document).ready(function(){
+            $('table tr').each(function(){
+                if($(this).find('td').eq(0).text() === 'State Unassigned'){
+                    $(this).css('display','none');
+                }
+            });
+        });
+        // $('table').sortable({items: 'tr:not(:first)'});
+
+        $('#txt_searchall').keyup(function(){
+            // Search Text
+            var search = $(this).val();
+        
+            // Hide all table tbody rows
+            $('table tbody tr').hide();
+        
+            // Count total search result
+            var len = $('table tbody tr:not(.notfound) td:contains("'+search+'")').length;
+        
+            if(len > 0){
+              // Searching text in columns and show match row
+              $('table tbody tr:not(.notfound) td:contains("'+search+'")').each(function(){
+                $(this).closest('tr').show();
+              });
+            }else{
+              $('.notfound').show();
+            }
+        
+          });
+
+        
     
  
 
@@ -64,37 +158,47 @@ const Covid = () => {
   return (
 
     <>
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"/> */}
         <section>
-            <h1 class="blinker">🔴Live</h1>
+            <h1 className="blinker">🔴Live</h1>
             <h2>Covid-19 LiveTracker </h2>
         </section>
         <div>
-            <table>
-                
+        <input type='text' id='txt_searchall' placeholder='Enter search text'></input>
+            <table id="mytable">
+            
+                <tbody>
+                <thead>
                 <tr>
-                    <td>State</td>
-                    <td>Recovered</td>
-                    <td>Confirmed</td>
-                    <td>Deaths</td>
-                    <td>Active</td>
-                    <td>LastUpdated</td>
+                    {/* <img src="https://img.icons8.com/material-outlined/24/000000/sort.png"/> */}
+                    <th id="nm">StateName</th>
+                    <th id="cnf" data-type="number">Confirmed<img src="https://img.icons8.com/material-outlined/24/000000/sort.png"/></th>
+                    <th id="act" data-type="number">Active<img src="https://img.icons8.com/material-outlined/24/000000/sort.png"/></th>
+                    <th id="rcv" data-type="number">Recovered<img src="https://img.icons8.com/material-outlined/24/000000/sort.png"/></th>
+                    <th id="dth" data-type="number">Deaths<img src="https://img.icons8.com/material-outlined/24/000000/sort.png"/></th>
+                    <th>LastUpdated</th>
                 </tr>
+                </thead>
                 
-                {data.map(dat => (
+                
+                {
+                    data.map(data => (
                     <tr>
-                    <td key={dat.id}>{dat.state}</td>
-                    <td key={dat.id}>{dat.recovered}</td>
-                    <td key={dat.id}>{dat.active}</td>
-                    <td key={dat.id}>{dat.confirmed}</td>
-                    <td key={dat.id}>{dat.deaths}</td>
-                    <td key={dat.id}>{dat.lastupdatedtime}</td>
+                        <td key={data.id}>{data.state}</td>
+                        <td key={data.id}>{data.confirmed}</td>
+                        <td key={data.id}>{data.active}</td>
+                        <td key={data.id}>{data.recovered}</td>
+                        <td key={data.id}>{data.deaths}</td>
+                        <td key={data.id}>{data.lastupdatedtime}</td>
                     </tr>
-                ))}
+                    ))
+                }
+                </tbody>
 
             </table>
         
             </div>
-            <div id="fx">
+            {/* <div id="fx">
                     <ul>
                         <li>StateName</li>
                         <li>TotalRecovered</li>
@@ -103,7 +207,7 @@ const Covid = () => {
                         <li>TotalActive</li>
                         <li>LastUpdateTime</li>
                     </ul>
-            </div>
+            </div> */}
     </>
   );
 }
